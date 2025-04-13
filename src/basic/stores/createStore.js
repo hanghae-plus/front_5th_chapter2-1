365e26a1b122227c119486b23c7d088b03f1c34a
HoJae Lee <hojae.lee@softly.ai>
2025-04-14 07:46:10 +0900

    feat: implement createStore

---
+++ src/basic/stores/createStore.js
@@ -0,0 +1,23 @@
+export function createStore(reducer, initialState) {
+  let state = initialState;
+  const listeners = [];
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    listeners.forEach((listener) => listener(state));
+  }
+
+  function subscribe(listener) {
+    listeners.push(listener);
+    return () => {
+      const idx = listeners.indexOf(listener);
+      if (idx > -1) listeners.splice(idx, 1);
+    };
+  }
+
+  function getState() {
+    return state;
+  }
+
+  return { dispatch, subscribe, getState };
+}

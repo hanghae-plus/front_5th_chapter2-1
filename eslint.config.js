46bfccc069ce4f0c78271fda6c61ed98c4443b3f
kutta97 <yhjin04170@gmail.com>
2025-04-14 00:18:56 +0900

    chore: eslint-config-prettier 패키지 의존성 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,10 @@
 import { defineConfig } from "eslint/config";
 import js from "@eslint/js";
 import globals from "globals";
-
+import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
   { files: ["**/*.{js}"], plugins: { js }, extends: ["js/recommended"] },
   { files: ["**/*.{js}"], languageOptions: { globals: globals.browser } },
+  eslintConfigPrettier,
 ]);

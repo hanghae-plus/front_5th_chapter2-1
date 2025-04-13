5c8c09fb9378872135b5719f8c9e1f314afc54c0
HoJae Lee <hojae.lee@softly.ai>
2025-04-14 07:46:21 +0900

    feat: implement cartStore

---
+++ src/basic/stores/cartStore.js
@@ -0,0 +1,13 @@
+import { cartReducer } from "../reducers/cartReducer";
+import { createStore } from "./createStore";
+
+const initialCartState = {
+  items: [],
+  lastSelected: 0,
+  totalPrice: 0,
+  totalQuantity: 0,
+  originalTotalPrice: 0,
+  bonusPoints: 0,
+};
+
+export const cartStore = createStore(cartReducer, initialCartState);

fec08c21b6e4e958b406f275427e530c89f34761
JiHwanLee <kdgosu53@gmail.com>
2025-04-13 18:15:15 +0900

    eslint 설치

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+// .eslintrc.js 예시
+module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+    },
+    extends: ["eslint:recommended"],
+    parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+    },
+    rules: {
+        // 원하는 규칙 추가
+    },
+};

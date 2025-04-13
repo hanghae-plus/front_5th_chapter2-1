3c9d509499e7fb71f21fe547a4054c4beb0c81c2
HoJae Lee <hojae.lee@softly.ai>
2025-04-14 05:55:31 +0900

    feat: install eslint import

--- eslint.config.js
+++ eslint.config.js
@@ -1,34 +1,37 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
+import importPlugin from "eslint-plugin-import";
 import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
-import { defineConfig } from "eslint/config";
+import pluginReact from "eslint-plugin-react";
 import globals from "globals";
 
-export default defineConfig([
+export default [
+  js.configs.recommended,
+  importPlugin.flatConfigs.recommended,
   {
-    ignorePatterns: ["package.json", "pnpm-lock.yaml"],
+    ignores: ["./src/main.original.js"],
   },
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
-    plugins: { js },
-    extends: ["js/recommended"],
-    rules: {
-      "prettier/prettier": "error",
-      "no-unused-vars": "error",
-      "no-var": "error",
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
     },
   },
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     languageOptions: { globals: globals.browser },
   },
+  pluginReact.configs.flat.recommended,
   {
     rules: {
       "prettier/prettier": "error",
       "no-unused-vars": "error",
       "no-var": "error",
+      "import/no-dynamic-require": "warn",
+      "import/no-nodejs-modules": "warn",
     },
   },
   eslintPluginPrettier,
   eslintConfigPrettier,
-]);
+];

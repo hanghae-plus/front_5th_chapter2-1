984899e8598533d07741e68e90d563834923d265
hanbeulYou <hanbeul.you@gmail.com>
2025-04-14 01:53:54 +0900

    chore: eslint와 prettier와 충돌나지 않게 config 추가

--- eslint.config.js
+++ eslint.config.js
@@ -1,11 +1,22 @@
-import { defineConfig } from "eslint/config";
-import js from "@eslint/js";
-import globals from "globals";
-import tseslint from "typescript-eslint";
-
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import prettierPlugin from 'eslint-plugin-prettier';
+import prettierConfig from 'eslint-config-prettier';
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
+  {
+    files: ['**/*.{js,ts}'],
+    plugins: { js, prettier: prettierPlugin },
+    rules: {
+      ...prettierPlugin.configs.recommended.rules,
+    },
+    languageOptions: {
+      globals: globals.browser,
+    },
+    settings: {},
+  },
   tseslint.configs.recommended,
+  prettierConfig,
 ]);

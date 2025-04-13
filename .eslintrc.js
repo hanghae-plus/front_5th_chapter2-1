00f2ec42a38d2aa682fc5b5de51f9cf0945031a2
anveloper <hitedin@gmail.com>
2025-04-13 15:42:44 +0900

    chore: eslint 설정파일 변경

--- .eslintrc.js
+++
@@ -1,7 +0,0 @@
-export default {
-  rules: {
-    "no-var": "error",
-    "no-unused-vars": ["error", { vars: "all", args: "after-used", ignoreRestSiblings: false }],
-    "prefer-const": ["error", { destructuring: "all", ignoreReadBeforeAssign: false }],
-  },
-};
---
+++ eslint.config.js
@@ -0,0 +1,24 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended, // eslint 기본 recommended 설정
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+    },
+    rules: {
+      "no-var": "error",
+      "no-unused-vars": ["warn", { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }],
+      "prefer-const": ["error", { destructuring: "all" }],
+      "no-const-assign": "error",
+      "no-redeclare": "error",
+      "no-use-before-define": "error",
+    },
+  },
+];

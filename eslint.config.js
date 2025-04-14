0b346fea0a40c5561a2a1fd28f03171a7d669f01
JiYu <eunjun1018@gmail.com>
2025-04-14 19:45:31 +0900

    eslint 수정

--- eslint.config.js
+++ eslint.config.js
@@ -21,12 +21,29 @@ export default defineConfig([
       'prettier/prettier': ['error', prettierConfig],
       'no-unused-vars': 'warn',
       'no-console': 'off',
-      //'no-var': 'error', // var 사용 금지
+      'no-var': 'error', // var 사용 금지
     },
   },
 ]);
 
-// export default defineConfig([
-//   { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"] },
-//   { files: ["**/*.{js,mjs,cjs}"], languageOptions: { globals: globals.browser } },
-// ]);
+// import globals from "globals";
+// import pluginJs from "@eslint/js";
+// import eslintConfigPrettier from "eslint-config-prettier";
+// import eslintPluginPrettier from "eslint-plugin-prettier/recommended";
+
+// /** @type {import('eslint').Linter.Config[]} */
+// export default [
+//   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
+//   pluginJs.configs.recommended,
+//   eslintPluginPrettier,
+//   eslintConfigPrettier,
+//   rules: {
+//     'no-var': 'error',
+//   }
+// ];
+
+
+
+// https://sung-98.tistory.com/114
+// https://velog.io/@nuyhes/Eslint-study-eslint.config.js-%EC%84%B8%ED%8C%85
+// https://velog.io/@iberis/%EC%82%AC%EB%82%B4-ESLint-Prettier-%EA%B3%B5%EC%9C%A0-%EC%84%A4%EC%A0%95-%ED%8C%A8%ED%82%A4%EC%A7%80-%EB%A7%8C%EB%93%A4%EA%B8%B0feat.-pnpm-eslint-9

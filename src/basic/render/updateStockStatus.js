a19eab1fcb1a7c993a5898ef7e4b27a230a67be3
hanbeulYou <hanbeul.you@gmail.com>
2025-04-15 01:23:48 +0900

    refactor: updateStockStatus 내부 함수 분리

--- src/basic/render/updateStockStatus.js
+++ src/basic/render/updateStockStatus.js
@@ -1,15 +1,18 @@
 import { PRODUCT_LIST } from '../consts';
 import { StockStatusContainerDOM } from '../ui';
+
+const getStockStatusMessage = (item) => {
+  if (item.quantity >= 5) return '';
+  const status =
+    item.quantity > 0 ? `재고 부족 (${item.quantity}개 남음)` : '품절';
+  return `${item.name}: ${status}\n`;
+};
+
 export const updateStockStatus = () => {
   const stockStatusContainer = StockStatusContainerDOM.get();
 
   const infoMessage = PRODUCT_LIST.reduce((acc, item) => {
-    if (item.quantity < 5) {
-      const status =
-        item.quantity > 0 ? `재고 부족 (${item.quantity}개 남음)` : '품절';
-      return `${acc}${item.name}: ${status}\n`;
-    }
-    return acc;
+    return acc + getStockStatusMessage(item);
   }, '');
 
   stockStatusContainer.textContent = infoMessage;

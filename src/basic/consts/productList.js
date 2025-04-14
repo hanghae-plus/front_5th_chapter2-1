334ee071444f406e444dff28278be4e3a84dfaa0
hanbeulYou <hanbeul.you@gmail.com>
2025-04-14 16:58:01 +0900

    refactor: productList 외부 상수화

---
+++ src/basic/consts/productList.js
@@ -0,0 +1,7 @@
+export const PRODUCT_LIST = [
+  { id: 'p1', name: '상품1', value: 10000, quantity: 50 },
+  { id: 'p2', name: '상품2', value: 20000, quantity: 30 },
+  { id: 'p3', name: '상품3', value: 30000, quantity: 20 },
+  { id: 'p4', name: '상품4', value: 15000, quantity: 0 },
+  { id: 'p5', name: '상품5', value: 25000, quantity: 10 },
+];

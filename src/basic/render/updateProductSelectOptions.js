0f5112c807dc81a6e8e4d24c09d173bfa5671294
hanbeulYou <hanbeul.you@gmail.com>
2025-04-15 01:22:15 +0900

    refactor: updateProductSelectOptions 내부 함수 분리

--- src/basic/render/updateProductSelectOptions.js
+++ src/basic/render/updateProductSelectOptions.js
@@ -1,15 +1,20 @@
 import { PRODUCT_LIST } from '../consts';
 import { ProductSelectDOM } from '../ui';
 
+const createOptionElement = (item) => {
+  const option = document.createElement('option');
+  option.value = item.id;
+  option.textContent = `${item.name} - ${item.value}원`;
+  if (item.quantity === 0) option.disabled = true;
+  return option;
+};
+
 export const updateProductSelectOptions = () => {
   const productSelect = ProductSelectDOM.get();
-  const newSelectOptions = PRODUCT_LIST.reduce((newOptions, item) => {
-    const option = document.createElement('option');
-    option.value = item.id;
-    option.textContent = `${item.name} - ${item.value}원`;
-    if (item.quantity === 0) option.disabled = true;
-    newOptions.appendChild(option);
-    return newOptions;
+
+  const newSelectOptions = PRODUCT_LIST.reduce((fragment, item) => {
+    fragment.appendChild(createOptionElement(item));
+    return fragment;
   }, document.createDocumentFragment());
 
   productSelect.replaceChildren(newSelectOptions);

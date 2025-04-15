0b5f43000d1702c0932d6dfcaadf90f0944266de
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-04-16 03:40:12 +0900

    Refactor: split lucky funtion

--- src/basic/utils/alerts/lucky.js
+++ src/basic/utils/alerts/lucky.js
@@ -2,12 +2,20 @@ import ITEMS from '../../constants/items';
 import updateSelectOptions from '../updateSelectOptions';
 import { LUCKY_DISCONT_RATE, LUCKY_PROBABILITY } from './constants';
 
-export const lucky = () => {
-  let luckyItem = ITEMS[Math.floor(Math.random() * ITEMS.length)];
+let luckyItem = ITEMS[Math.floor(Math.random() * ITEMS.length)];
 
-  if (Math.random() < LUCKY_PROBABILITY && luckyItem.stock > 0) {
-    luckyItem.price = Math.round(luckyItem.price * LUCKY_DISCONT_RATE);
+export const lucky = () => {
+  if (isLucky(luckyItem)) {
+    luckyItem.price = calculateLuckyPrice();
     alert('번개세일! ' + luckyItem.name + '이(가) 20% 할인 중입니다!');
     updateSelectOptions();
   }
+};
+
+const isLucky = () => {
+  return Math.random() < LUCKY_PROBABILITY && luckyItem.stock > 0;
+};
+
+const calculateLuckyPrice = () => {
+  return Math.round(luckyItem.price * LUCKY_DISCONT_RATE);
 };

d98ac89fa3baeed34a2e922e3f1f27735750fb93
dlsxody1 <dlsxody1@naver.com>
2025-04-15 20:04:47 +0900

    chore : eslint 버전으로 인해 eslintrc -> eslint.config.js 로 파일 형식 변경 + 추가적인 세팅

---
+++ eslint.config.js
@@ -0,0 +1,37 @@
+import eslint from "@eslint/js";
+import prettierPlugin from "eslint-plugin-prettier";
+import prettier from "eslint-config-prettier";
+
+export default [
+  eslint.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: "latest",
+      sourceType: "module",
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true
+        }
+      }
+    },
+    plugins: {
+      prettier: prettierPlugin
+    },
+    rules: {
+      "prettier/prettier": "error",
+      "no-console": ["warn", { allow: ["warn", "error"] }],
+      "no-unused-vars": "warn",
+      "no-debugger": "warn",
+      "no-var": "error",
+      "prefer-spread": "error",
+      "prefer-const": "error"
+    }
+  },
+  {
+    files: ["**/*.test.js", "**/*.test.ts", "**/*.spec.js", "**/*.spec.ts"],
+    rules: {
+      "no-console": "off"
+    }
+  },
+  prettier // ESLint와 Prettier 충돌 방지 (항상 마지막에 위치)
+];

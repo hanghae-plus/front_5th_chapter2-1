77786f65ad228d8e1ddb31f3cb4e564d14229bb1
Lee MinGi <mingi3442@gmail.com>
2025-04-15 21:06:24 +0900

    🔧 Update configuration

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	base: import.meta.env.PROD ? '/front_5th_chapter2-1/' : '',
+	base: process.env.NODE_ENV === 'production' ? '/front_5th_chapter2-1/' : '',
 	test: {
 		globals: true,
 		environment: 'jsdom',

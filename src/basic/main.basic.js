a3163af10b72ab70a63a05f860ea722a1b2a4b3d
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-04-16 04:43:34 +0900

    Refactor: events폴더 생성[basic 하위 폴더] handleAddCart분리 (hadleAddCartButton->handleAddCart)

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -2,7 +2,7 @@ import updateSelectOptions from './utils/updateSelectOptions';
 import render from './render';
 import updateCart from './utils/updateCart';
 import alerts from './utils/alerts/alerts';
-import eventHandler from './utils/eventHandler';
+import eventHandler from './events/eventHandler';
 
 const main = () => {
   updateSelectOptions();

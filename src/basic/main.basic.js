1ae6c8fd7fa3095106c01590c99f670bd020ba70
chloekim <chloe@perfitt.io>
2025-04-16 13:50:04 +0900

    refactor: handleCartAction함수 분리 및 변수명 수정

--- src/basic/main.basic.js
+++ src/basic/main.basic.js
@@ -1,18 +1,25 @@
 
 import { handleAddToCart } from "./logic/add-to-cart";
 import { calculateCart } from "./logic/calculate-cart";
+import { handleCartItemAction } from "./logic/cart-item-action";
 import { startLastSaleTimer, startLuckySaleTimer } from "./logic/sale-timer";
 import { CartUiTemplate } from "./ui/cart-template";
 
 const lastSaleRef = { current: null }; // 외부에서 선택값을 계속 업데이트함
 
-const $addBtn = document.getElementById('add-to-cart');
-const $cartDisplay =document.getElementById('cart-items');
 
 function main() {
   //요소 생성
   const $root = document.getElementById('app');
   $root.innerHTML = CartUiTemplate();
+
+  //요소 찾기
+  const $addBtn = document.getElementById('add-to-cart');
+  const $cartDisplay =document.getElementById('cart-items');
+
+  //이벤트 핸들러 등록
+  $addBtn?.addEventListener('click', handleAddToCart(lastSaleRef) );
+  $cartDisplay?.addEventListener('click', handleCartItemAction);
 
   //계산 함수
   calculateCart();
@@ -24,32 +31,3 @@ function main() {
 
 main();
 
-$addBtn?.addEventListener('click', handleAddToCart(lastSaleRef) );
-
-
-$cartDisplay?.addEventListener('click', function (event) {
-  var tgt=event.target;
-  if(tgt.classList.contains('quantity-change') || tgt.classList.contains('remove-item')) {
-    var prodId=tgt.dataset.productId;
-    var itemElem=document.getElementById(prodId);
-    var prod=prodList.find(function (p) { return p.id === prodId; });
-    if(tgt.classList.contains('quantity-change')) {
-      var qtyChange=parseInt(tgt.dataset.change);
-      var newQty=parseInt(itemElem.querySelector('span').textContent.split('x ')[1]) + qtyChange;
-      if(newQty > 0 && newQty <= prod.q + parseInt(itemElem.querySelector('span').textContent.split('x ')[1])) {
-        itemElem.querySelector('span').textContent=itemElem.querySelector('span').textContent.split('x ')[0] + 'x ' + newQty;
-        prod.q -= qtyChange;
-      } else if(newQty <= 0) {
-        itemElem.remove();
-        prod.q -= qtyChange;
-      } else {
-        alert('재고가 부족합니다.');
-      }
-    } else if(tgt.classList.contains('remove-item')) {
-      var remQty=parseInt(itemElem.querySelector('span').textContent.split('x ')[1]);
-      prod.q += remQty;
-      itemElem.remove();
-    }
-    calcCart();
-  }
-});

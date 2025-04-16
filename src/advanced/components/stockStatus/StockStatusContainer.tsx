c888503847e0b03c13f28715d39828977196e886
hanbeulYou <hanbeul.you@gmail.com>
2025-04-17 03:29:33 +0900

    fix: StockStatusContainer에 children props 받도록 변경

--- src/advanced/components/stockStatus/StockStatusContainer.tsx
+++ src/advanced/components/stockStatus/StockStatusContainer.tsx
@@ -1,11 +1,17 @@
 import type React from "react";
 import { STYLES, DOM_IDS } from "@/basic/consts";
 
-export const StockStatusContainer: React.FC = () => {
+interface StockStatusContainerProps {
+  children: React.ReactNode;
+}
+
+export const StockStatusContainer: React.FC<StockStatusContainerProps> = ({ children }) => {
   return (
     <div 
       id={DOM_IDS.PRODUCT.STOCK_STATUS}
       className={STYLES.TEXT.STOCK}
-    />
+    >
+      {children}
+    </div>
   );
 };

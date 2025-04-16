66d9a504f9dbbae75cd7b4b15b9970145d305473
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-04-17 00:36:19 +0900

    Feat: components생성, App에서 호출, constants/items 생성

--- src/advanced/App.tsx
+++ src/advanced/App.tsx
@@ -1,7 +1,26 @@
 import React from 'react';
+import Title from './components/Title';
+import Cart from './components/Cart';
+import CartTotal from './components/CartTotal';
+import ItemSelect from './components/ItemSelect';
+import AddCartButton from './components/AddCartButton';
+import Stock from './components/Stock';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <div className="bg-gray-100 p-8">
+        <div className="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl p-8">
+          <Title />
+          <Cart />
+          <CartTotal />
+          <ItemSelect />
+          <AddCartButton />
+          <Stock />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default App;
---
+++ src/advanced/components/AddCartButton.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AddCartButton = () => {
+  return <div>AddCartButton</div>;
+};
+
+export default AddCartButton;

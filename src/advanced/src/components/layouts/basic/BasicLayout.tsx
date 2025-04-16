3cf6caa3ab2688a647f6246bb83de4442e714ba6
상은 <1-blue98@naver.com>
2025-04-16 19:43:02 +0900

    refactor:[advanced] 기본 레이아웃 변경

--- src/advanced/src/components/layouts/basic/BasicLayout.tsx
+++ src/advanced/src/components/layouts/basic/BasicLayout.tsx
@@ -1,10 +1,15 @@
+import React from "react";
+
+import { cn } from "#advanced/libs";
 import Header from "#advanced/components/layouts/Header";
 import Main from "#advanced/components/layouts/Main";
 import Footer from "#advanced/components/layouts/Footer";
 
-const BasicLayout: React.FC<React.PropsWithChildren> = ({ children }) => {
+interface IProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+const BasicLayout: React.FC<React.PropsWithChildren<IProps>> = ({ children, className, ...props }) => {
   return (
-    <div className="bg-gray-100 min-h-screen flex">
+    <div {...props} className={cn("bg-gray-100 min-h-screen", className)}>
       <Header />
       <Main>{children}</Main>
       <Footer />

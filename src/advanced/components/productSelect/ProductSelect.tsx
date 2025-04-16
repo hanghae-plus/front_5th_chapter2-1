9b16cfe42a881f86f7f2e537e11d6490cafdec22
hanbeulYou <hanbeul.you@gmail.com>
2025-04-17 04:57:38 +0900

    feat: ProductSelect onChange 기능 추가

--- src/advanced/components/productSelect/ProductSelect.tsx
+++ src/advanced/components/productSelect/ProductSelect.tsx
@@ -1,14 +1,17 @@
 import { DOM_IDS, STYLES } from "@/basic/consts";
 import { formatProductOption } from "@/advanced/utils";
-import { PRODUCT_LIST } from "@/basic/consts";
+import { useProduct } from "@/advanced/context";
 
 export const ProductSelect: React.FC = () => {
+  const { productList, setSelectedProductId } = useProduct();
+
   return (
     <select 
       id={DOM_IDS.PRODUCT.SELECT} 
       className={STYLES.FORM.SELECT}
+      onChange={(e) => setSelectedProductId(e.target.value)}
     >
-      {PRODUCT_LIST.map((item) => (
+      {productList.map((item) => (
         <option
           key={item.id}
           value={item.id}

e69c48e01e30ea2a21534012e4e884b060c0f097
sungchan.kim <sungchan.kim@datastreams.co.kr>
2025-04-17 07:57:35 +0900

    🚀 deploy: 배포 관련 수정

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,21 @@
 import { defineConfig } from 'vitest/config';
+import { resolve } from "path";
 
 export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: 'src/setupTests.js'
+  },
+  base:
+      process.env.NODE_ENV === "production" ? "/front_5th_chapter2-1/" : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.basic.html"),
+        advanced: resolve(__dirname, "index.advanced.html"),
+        notFound: resolve(__dirname, "404.html"),
+      },
+    },
   },
 })

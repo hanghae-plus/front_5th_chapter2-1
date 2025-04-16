2c7fb424751777a963c3c4bdffd2ae26529d1a80
Lee MinGi <mingi3442@gmail.com>
2025-04-16 18:57:12 +0900

    🔧 Add react, ts

---
+++ src/advanced/main.advanced.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './src/apps/styles/reset.css';
+
+ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
+	<React.StrictMode>
+		<div>HI</div>
+	</React.StrictMode>,
+);
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
+import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	base: process.env.NODE_ENV === 'production' ? '/front_5th_chapter2-1/' : '',
+	plugins: [react()],
 	test: {
 		globals: true,
 		environment: 'jsdom',

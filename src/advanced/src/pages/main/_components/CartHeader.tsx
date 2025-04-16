4d873d350918c10979c3c30b5d4936d159b7746d
상은 <1-blue98@naver.com>
2025-04-16 21:13:00 +0900

    refactor:[advanced] 장바구니 헤더 변경

--- src/advanced/src/pages/main/_components/CartHeader.tsx
+++ src/advanced/src/pages/main/_components/CartHeader.tsx
@@ -1,7 +1,7 @@
 const CartHeader: React.FC = () => {
   return (
-    <section>
-      <h6 className="text-2xl font-bold">장바구니 제목</h6>
+    <section className="flex flex-col items-center justify-center gap-2">
+      <h6 className="text-2xl font-bold">🕹️ 아무튼 장바구니 🕹️</h6>
       <p className="text-xs text-gray-700">장바구니에 담긴 상품을 확인할 수 있습니다.</p>
     </section>
   );

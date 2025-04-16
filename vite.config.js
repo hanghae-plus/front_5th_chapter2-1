80fb8da2b8e4377ca24509c7200bcc4f4d1d7a81
EZONE <meowoof01@gmail.com>
2025-04-17 04:14:28 +0900

    chore : 심화과제 시작을 위한 react 세팅....

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    extensions: ['.js', '.jsx', '.ts', '.tsx'],
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
+    setupFiles: './src/setupTests.js',
   },
-})
+});

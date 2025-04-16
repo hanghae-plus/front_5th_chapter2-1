a2b08e5670205f18ac75e88fe20776498d053b64
hanbeulYou <hanbeul.you@gmail.com>
2025-04-17 03:33:51 +0900

    feat: MainPage에 생성된 컴포넌트들 렌더 추가

--- src/advanced/pages/MainPage.tsx
+++ src/advanced/pages/MainPage.tsx
@@ -1,10 +1,13 @@
-import { Layout, ProductSelect, AddButton } from "@/advanced/components";
+import { Layout, ProductSelect, AddButton, StockStatusContainer, CartItemListContainer, TotalAmountContainer } from "@/advanced/components";
 
 export const MainPage = () => {
   return (
     <Layout>
+      <CartItemListContainer><div>장바구니</div></CartItemListContainer>
+      <TotalAmountContainer><div>총 금액</div></TotalAmountContainer>
       <ProductSelect/>
       <AddButton />
+      <StockStatusContainer><div>재고</div></StockStatusContainer>
     </Layout>
   );
 };

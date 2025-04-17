27cc30a0b5e63b59e854403f0f8182af33c72c1d
상은 <1-blue98@naver.com>
2025-04-18 00:26:43 +0900

    refactor:[advanced] 의미없는 주석 제거

--- src/advanced/src/pages/main/_constants/products.ts
+++ src/advanced/src/pages/main/_constants/products.ts
@@ -1,6 +1,5 @@
 import type { IProduct } from "#advanced/pages/main/_types";
 
-// faker.js를 한국어 로케일로 설정하여 한국어 데이터 생성
 export const PRODUCTS: IProduct[] = Array(10)
   .fill(null)
   .map((_, index) => ({

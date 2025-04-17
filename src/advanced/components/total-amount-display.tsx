cea1db618401fb3a203869a0cfe6de3c2eb711e0
Doh Kim <ehgud456456@naver.com>
2025-04-17 11:53:12 +0900

    fix: 불필요하게 분리한 컴포넌트 병합

--- src/advanced/components/total-amount-display.tsx
+++ src/advanced/components/total-amount-display.tsx
@@ -1,38 +1,19 @@
 import { useProductList } from '../context/product';
 
 export const TotalAmountDisplay = () => {
-  return (
-    <div id="cart-total" className="text-lg font-bold my-4">
-      <TotalAmount />
-      <Discounts />
-      <Points />
-    </div>
-  );
-};
-
-export const TotalAmount = () => {
-  const { totalAmount } = useProductList();
-  return `총액: ${totalAmount}원`;
-};
-
-export const Discounts = () => {
-  const { avgDiscountRate } = useProductList();
-
-  return (
-    avgDiscountRate > 0 && (
-      <span className="text-red-500 ml-2">
-        ({(avgDiscountRate * 100).toFixed(1)}% 할인 적용)
-      </span>
-    )
-  );
-};
-
-export const Points = () => {
-  const { totalAmount } = useProductList();
+  const { totalAmount, avgDiscountRate } = useProductList();
   const points = Math.floor(totalAmount / 10000);
   return (
-    <span id="loyalty-points" className="text-blue-500 ml-2">
-      (포인트: {points})
-    </span>
+    <div id="cart-total" className="text-lg font-bold my-4">
+      총액: {totalAmount}원
+      {avgDiscountRate > 0 && (
+        <span className="text-red-500 ml-2">
+          ({(avgDiscountRate * 100).toFixed(1)}% 할인 적용)
+        </span>
+      )}
+      <span id="loyalty-points" className="text-blue-500 ml-2">
+        (포인트: {points})
+      </span>
+    </div>
   );
 };

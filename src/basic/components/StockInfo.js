7e7ffa491fa1bbf7940a399fa6aa568d4e458785
dlsxody1 <dlsxody1@naver.com>
2025-04-17 18:30:55 +0900

    chore : stock 렌더러와 stock 분리

--- src/basic/components/StockInfo.js
+++ src/basic/components/StockInfo.js
@@ -1,23 +1,3 @@
-import { getStore } from "../store/store.js";
-
 export const StockInfo = () => {
   return `<div id="stock-status" class="text-sm text-gray-500 mt-2"></div>`;
-};
-
-export const updateStockInfo = () => {
-  const { products } = getStore();
-  const stockElement = document.getElementById("stock-status");
-  if (!stockElement) return;
-
-  let infoMessage = "";
-
-  products.forEach((product) => {
-    if (product.q < 5) {
-      infoMessage += `${product.name}: ${
-        product.q > 0 ? `재고 부족 (${product.q}개 남음)` : "품절"
-      }\n`;
-    }
-  });
-
-  stockElement.textContent = infoMessage;
 };

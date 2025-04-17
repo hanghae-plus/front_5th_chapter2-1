50d72e4b0849458cb61fa99dd490295ae8b63603
SeongYoonMin <namo4862@naver.com>
2025-04-18 01:23:04 +0900

    feat: 전역상태관리를 위한 shoppingState 객체 추가

---
+++ src/basic/store/state.js
@@ -0,0 +1,10 @@
+import PRODUCT_LIST from "./constants/constant.js";
+//let cartList, addProductBtn, cartContainer, totalPrice, stockInfo; // DOM Elements임
+
+export const shoppingState = {
+  productList: PRODUCT_LIST,
+  lastSel: 0,
+  bonusPoints: 0,
+  totalAmount: 0,
+  itemCount: 0,
+}

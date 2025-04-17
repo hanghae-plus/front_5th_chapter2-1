b10f0e328f0ccd3b7a6ec4c066d5a3792ad3dd7e
hyunjin <71summernight@naver.com>
2025-04-18 02:27:24 +0900

    vite config test 누락 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 // vite.config.ts
 import react from '@vitejs/plugin-react';
 import path from 'path';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   base: '/front_5th_chapter2-1/',
@@ -18,5 +18,10 @@ export default defineConfig({
         advanced: path.resolve(__dirname, 'index.advanced.html'),
       },
     },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'src/setupTests.js',
   },
 });

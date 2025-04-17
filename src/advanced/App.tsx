955ff1364072c2ece7aba67e5e32f1718c100ac0
chloekim <chloe@perfitt.io>
2025-04-17 21:43:34 +0900

    feat:라우팅 추가

--- src/advanced/App.tsx
+++ src/advanced/App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { CartPage } from "./pages/cart-page";
 
 const App = () => {
   return (
-    <div className="p-6">
-      <h1 className="text-2xl font-bold">🛒 장바구니 앱</h1>
-      {/* 여기에 CartPage 또는 메인 컴포넌트 넣기 */}
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<CartPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 };
 

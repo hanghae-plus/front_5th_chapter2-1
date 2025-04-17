b31fef3cb3cf83e72dba1b9aec91373a122b1af1
JiYu <eunjun1018@gmail.com>
2025-04-17 12:47:01 +0900

    deploy basic

--- src/basic/vite.config.js
+++ src/basic/vite.config.js
@@ -1,11 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
+import { resolve } from 'path'
 
 export default defineConfig({
-  base: '/hanghae99-chap4/',
+  root: 'src/basic',
+  base: '/hanghae99-chap4/basic/',
   build: {
     rollupOptions: {
-      input: 'src/basic/index.basic.html', // 진입점
+      input: resolve(__dirname, 'index.html'), // 기본 진입점
     },
-    outDir: 'docs/basic',
-  }
-});
+    outDir: '../../../docs/basic', // 빌드 결과를 docs/basic에 저장
+    emptyOutDir: true,
+  },
+})

4b0db5fb7944f57e61931cadd8ecad598a9c85bd
hanbeulYou <hanbeul.you@gmail.com>
2025-04-18 06:07:58 +0900

    chore: 배포 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,12 @@ import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
 
+export const BASE_PATH =
+  process.env.NODE_ENV === 'production' ? '/front_5th_chapter2-1/' : '/';
+
 export default defineConfig({
   plugins: [react()],
+  base: BASE_PATH,
   resolve: {
     alias: {
       '@': resolve(__dirname, './src'),

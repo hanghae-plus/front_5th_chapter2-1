e9511663d1aab0d9f42e45843e8eb33f1297155a
Jeong-wonho <kallzzang@naver.com>
2025-04-18 02:51:41 +0900

    vite.config.js 수정2

--- vite.config.js
+++ vite.config.js
@@ -7,19 +7,18 @@ export default defineConfig(({ mode }) => {
   // 1. 현재 mode에 맞는 .env 파일을 불러옵니다.
   //    세 번째 인자는 prefix 필터, ''로 두면 VITE_ 로 시작하는 모든 변수를 로드
   const base = mode === "production" ? "/front_5th_chapter2-1/" : "/";
-
   return {
     // 2. base URL 설정
     base: base,
-
     plugins: [react()],
     build: {
-      outDir: 'dist',
       rollupOptions: {
         input: {
-          main: "./index.advanced.html",
+          main: resolve(__dirname, 'index.basic.html'),
+          advanced: resolve(__dirname, 'index.advanced.html'),
         },
       },
+      outDir: 'dist',
     },
 
     test: {

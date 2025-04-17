acd22efd22c74bd221c0934393387fa145d29c71
anveloper <hitedin@gmail.com>
2025-04-17 23:21:48 +0900

    chore: 배포 세팅 완료

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,29 @@
+import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { defineConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+const isCI = process.env.CI === "true";
 
-export default defineConfig({
-  resolve: { alias: { "@": path.resolve(__dirname, "./src") } },
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests.js",
-  },
-});
+export default mergeConfig(
+  defineConfig({
+    base: isCI ? "" : "/front_5th_chapter2-1/",
+    plugins: [react()],
+    resolve: { alias: { "@": path.resolve(__dirname, "src") } },
+    build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, "index.html"),
+          basic: path.resolve(__dirname, "index.basic.html"),
+          advanced: path.resolve(__dirname, "index.advanced.html"),
+        },
+      },
+    },
+  }),
+  defineTestConfig({
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "src/setupTests.js",
+    },
+  })
+);

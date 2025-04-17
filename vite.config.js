4596c5434a52d4e5f28cb7c740ba2f5a2318b387
Wonny <h.wonny11@gmail.com>
2025-04-18 05:44:26 +0900

    deploy setting

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
-import { defineConfig } from "vitest/config";
-
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests.js"
-  }
-});
+import { defineConfig as defineTestConfig, mergeConfig } from "vitest/config";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
+export default mergeConfig(
+  defineConfig({
+    plugins: [react()],
+    base: process.env.NODE_ENV === "production" ? "/front_5th_chapter2-1/" : "/",
+    build: {
+      rollupOptions: {
+        input: {
+          main: path.resolve(__dirname, "index.html"),
+          basic: path.resolve(__dirname, "index.basic.html"),
+          advanced: path.resolve(__dirname, "index.advanced.html")
+        }
+      }
+    }
+  }),
+  defineTestConfig({
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "./src/setupTests.js"
+    }
+  })
+);

6a3a3454da1dec9c1e59fc3eb8faf00c9a96aed3
노 기 훈 <66203019+Nohgh@users.noreply.github.com>
2025-04-18 05:44:21 +0900

    Chore: gh pages deploy

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,25 @@
-import { defineConfig } from 'vitest/config';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import { defineConfig } from 'vite';
+import { BASE_URL } from './src/baseUrl';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: BASE_URL + '/index.basic.html',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.basic.html'),
+      },
+    },
+    outDir: 'dist',
+    emptyOutDir: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
+    setupFiles: 'src/setupTests.js',
   },
-})
+});

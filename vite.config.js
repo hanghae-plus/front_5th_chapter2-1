e4db7bce3f9fdf70d6be3fd7294310971e52c9e5
JiYu <eunjun1018@gmail.com>
2025-04-17 13:57:05 +0900

    deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   base: '/hanghae99-chap4/basic/', // GitHub Pages에서의 배포 경로
   build: {
+    rollupOptions: {
+      input: resolve(__dirname, 'basic/index.html'),  // index.html 경로 명시
+    },
     outDir: 'docs/basic',  // 빌드된 파일들이 docs/basic 폴더에 저장되게끔
     emptyOutDir: true,     // 빌드 전에 기존 파일들 삭제
   },

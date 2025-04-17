74255ba9658d11a9ae9f6484cce6646c5d43d717
chaaerim <cofla411@ewhain.net>
2025-04-17 23:57:59 +0900

    feat: 배포 관련 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,28 @@
-import { defineConfig } from 'vitest/config';
+import { resolve } from "node:path";
+import { loadEnv } from "vite";
+// vite.config.ts
+import { defineConfig } from "vitest/config";
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
-  },
-})
+export default defineConfig(({ mode }) => {
+  // mode 에 따라 .env 파일에서 VITE_HOSTNAME 불러오기
+  const env = loadEnv(mode, process.cwd(), "");
+
+  return {
+    root: ".",
+    base: env.VITE_HOSTNAME || "/",
+
+    build: {
+      rollupOptions: {
+        input: {
+          main: resolve(__dirname, "index.basic.html"),
+        },
+      },
+    },
+
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "src/setupTests.js",
+    },
+  };
+});

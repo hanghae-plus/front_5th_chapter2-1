d7745e89e1fefa738b83b40757bf677794d2e379
XXELL-8 <cell8pepper@gmail.com>
2025-04-17 23:45:14 +0900

    Chore: Git Pages 배포 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,23 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
+  base: '/front_5th_chapter2-1/',
   plugins: [
     react({
       include: '**/src/advanced/**',
     }),
   ],
+  build: {
+    rollupOptions: {
+      input: {
+        basic: resolve(__dirname, 'index.basic.html'),
+        advanced: resolve(__dirname, 'index.advanced.html'),
+      },
+    },
+    outDir: 'dist',
+  },
   test: {
     globals: true,
     environment: 'jsdom',

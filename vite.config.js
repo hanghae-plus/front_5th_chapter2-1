4e68850d29a366a2381d103f82c00502b2710a28
이흥수 [Frontend] <heungsoo1@gmail.com>
2025-04-18 02:25:17 +0900

    feat: 배포 설정 추가

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,26 @@
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [react()],
+  base: '/front_5th_chapter2-1/',
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
+    setupFiles: 'src/setupTests.js',
   },
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: 'index.html',
+        basic: 'index.basic.html',
+        advanced: 'index.advanced.html',
+      },
+      output: {
+        entryFileNames: () => {
+          return `assets/[name]-[hash].js`;
+        },
+      },
+    },
+  },
+});

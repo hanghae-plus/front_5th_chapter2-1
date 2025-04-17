03321639ee77c1490e6462d55c8002911771ea05
chloekim <chloe@perfitt.io>
2025-04-18 01:19:38 +0900

    chore:배포 설정

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests.js",
-  },
+export default defineConfig(({ mode }) => {
+  const isProd = mode === "production";
+  const BASE_PATH = isProd ? "/front_5th_chapter2-1/" : "/";
+
+  return {
+    plugins: [react()],
+    base: BASE_PATH,
+    test: {
+      globals: true,
+      environment: "jsdom",
+      setupFiles: "src/setupTests.js",
+    },
+  };
 });

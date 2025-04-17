2de6303fa1a9b6d538fb1b9423fa5609dd36a881
hyunjin <71summernight@naver.com>
2025-04-18 02:16:19 +0900

    배포 설정

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/front_5th_chapter2-1/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),

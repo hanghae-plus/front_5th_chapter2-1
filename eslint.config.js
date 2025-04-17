27ea405a17adad5d99acd7b010247a33af9ab32d
XXELL-8 <cell8pepper@gmail.com>
2025-04-17 21:10:53 +0900

    Chore: React + TypeScript 사용을 위한 의존성 및 config 업데이트

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,41 @@
-import { defineConfig } from 'eslint/config'
-import js from '@eslint/js'
-import globals from 'globals'
-import tseslint from 'typescript-eslint'
-import pluginReact from 'eslint-plugin-react'
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import tseslint from '@typescript-eslint/eslint-plugin';
+import pluginReact from 'eslint-plugin-react';
+import pluginReactHooks from 'eslint-plugin-react-hooks';
 
-export default defineConfig([
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], plugins: { js }, extends: ['js/recommended', 'prettier'] },
-  { files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
-])
+export default defineConfig({
+  overrides: [
+    {
+      files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+      plugins: {
+        js,
+        'react': pluginReact,
+        '@typescript-eslint': tseslint,
+        'react-hooks': pluginReactHooks,
+      },
+      extends: ['eslint:recommended', 'plugin:react/recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+      rules: {
+        'react/prop-types': 'off',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        'react/jsx-uses-react': 'off',
+        'react/react-in-jsx-scope': 'off',
+        'react/jsx-max-depth': ['warn', { max: 5 }],
+      },
+    },
+    {
+      files: ['**/*.ts', '**/*.tsx'],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+  ],
+});
--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,15 @@
 import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  plugins: [
+    react({
+      include: '**/src/advanced/**',
+    }),
+  ],
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: 'src/setupTests.js'
+    setupFiles: 'src/setupTests.js',
   },
-})
+});

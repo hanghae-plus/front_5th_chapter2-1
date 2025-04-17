f6c5c2fc2f92526b363914e1c48f6e77d6c9160a
HoJae Lee <hojae.lee@softly.ai>
2025-04-17 23:38:26 +0900

    feat: install gh pages

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,14 @@ import { defineConfig } from "vitest/config";
 import path from "node:path";
 
 export default defineConfig({
+  base: process.env.NODE_ENV === "production" ? "/front_5th_chapter2-1/" : "/",
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.advanced.html"),
+      },
+    },
+  },
   resolve: {
     alias: {
       "@advanced": path.resolve(__dirname, "./src/advanced"),

0644a562040494dfbbd68c902a586bdf47f3d023
DohyungKim <ehgud456456@naver.com>
2025-04-17 22:29:07 +0900

    feat: build 및 배포를 위한 setting

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,20 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/front_5th_chapter2-1/' : '/',
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        basic: resolve(__dirname, 'index.basic.html'),
+        advanced: resolve(__dirname, 'index.advanced.html'),
+      },
+    },
+  },
+
   test: {
     globals: true,
     environment: 'jsdom',

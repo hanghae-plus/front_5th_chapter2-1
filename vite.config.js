16853c328ae2fbcd86803f370970d731ccc2b2d3
devchaeyoung <chaemaa00@gmail.com>
2025-04-18 05:00:02 +0900

    🚀chore: viteconfig삭제

--- vite.config.js
+++ vite.config.js
@@ -1,34 +1,9 @@
-import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vitest/config';
 
-export default ({ mode }) => {
-  const base = mode === 'production' ? '/front_5th_chapter1-3/' : '/';
-
-  return mergeConfig(
-    defineConfig({
-      plugins: [react()],
-      base,
-      build: {
-        rollupOptions: {
-          input: {
-            main: './index.html',
-            notFound: './404.html',
-          },
-        },
-      },
-    }),
-
-    defineTestConfig({
-      test: {
-        globals: true,
-        environment: 'jsdom',
-        setupFiles: './src/setupTests.ts',
-        coverage: {
-          reportsDirectory: './.coverage',
-          reporter: ['lcov', 'json', 'json-summary'],
-        },
-      },
-    }),
-  );
-};
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: 'src/setupTests.js',
+  },
+});

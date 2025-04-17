ff3aa92035c183637d4ac53e53155f2b21b17749
JiYu <eunjun1018@gmail.com>
2025-04-17 12:19:41 +0900

    deploy basic

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,13 @@
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
+  base: '/hanghae99-chap4/',
+  build: {
+    rollupOptions: {
+      input: 'src/basic/index.basic.html', // 진입점
+    },
+    outDir: 'docs/basic', // 빌드 결과물을 docs/basic 폴더에 생성
+  },
   test: {
     globals: true,
     environment: 'jsdom',

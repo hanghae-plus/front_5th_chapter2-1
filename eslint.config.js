4fa2bae50a9d29ce4eb0f2e7e46a888a641c260d
JiHwanLee <kdgosu53@gmail.com>
2025-04-18 04:30:37 +0900

    prettierrc, eslint 다시 수정

--- eslint.config.js
+++ eslint.config.js
@@ -2,8 +2,10 @@ import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
 
-
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,ts}"], languageOptions: { globals: globals.browser } },
-  tseslint.configs.recommended,
+    {
+        files: ["**/*.{js,mjs,cjs,ts}"],
+        languageOptions: { globals: globals.browser },
+    },
+    tseslint.configs.recommended,
 ]);

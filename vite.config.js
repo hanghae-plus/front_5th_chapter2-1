c8b34e9024a0350d0ac504d38b15d3b2359c6050
JiYu <eunjun1018@gmail.com>
2025-04-17 14:02:35 +0900

    so tired

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
     rollupOptions: {
       input: resolve(__dirname, 'basic/index.html'),  // index.html 경로 명시
     },
-    outDir: 'docs/basic',  // 빌드된 파일들이 docs/basic 폴더에 저장되게끔
+    outDir: 'docs',  // 빌드된 파일들이 docs/basic 폴더에 저장되게끔
     emptyOutDir: true,     // 빌드 전에 기존 파일들 삭제
   },
 });

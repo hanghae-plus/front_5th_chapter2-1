4bff27ee2553d7bf67c5890dd6d513196061260f
JiHwanLee <kdgosu53@gmail.com>
2025-04-17 13:22:52 +0900

    공통 type설정 및 productData파일 만듦

--- src/advanced/data/productData.ts
+++ src/advanced/data/productData.ts
@@ -1,14 +1,9 @@
-interface ProductType {
-    id: string;
-    name: string;
-    val: number;
-    q: number;
-}
+import { Product } from "../types";
 
-export const PRODUCT_LIST: ProductType[] = [
-    { id: "p1", name: "상품1", val: 1000, q: 10 },
-    { id: "p2", name: "상품2", val: 2000, q: 5 },
-    { id: "p3", name: "상품3", val: 3000, q: 15 },
-    { id: "p4", name: "상품4", val: 4000, q: 3 },
-    { id: "p5", name: "상품5", val: 5000, q: 7 },
+export const PRODUCT_LIST: Product[] = [
+    { id: "p1", name: "상품1", val: 10000, q: 50 },
+    { id: "p2", name: "상품2", val: 20000, q: 30 },
+    { id: "p3", name: "상품3", val: 30000, q: 20 },
+    { id: "p4", name: "상품4", val: 15000, q: 0 },
+    { id: "p5", name: "상품5", val: 25000, q: 10 },
 ];

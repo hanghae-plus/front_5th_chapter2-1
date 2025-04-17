2f3e177d9cad27cc70312bcd8bcc58944551a364
mnxmnz <minzidev@gmail.com>
2025-04-17 17:16:07 +0900

    refactor: 선택 박스 옵션 비활성화 로직 간소화

--- src/basic/updateSelectBoxOption.js
+++ src/basic/updateSelectBoxOption.js
@@ -2,13 +2,9 @@ const updateSelectBoxOptions = (productSelectBox, products) => {
   productSelectBox.innerHTML = '';
   products.forEach(({ id, name, price, quantity }) => {
     const option = document.createElement('option');
-
     option.value = id;
     option.textContent = `${name} - ${price}원`;
-
-    if (quantity === 0) {
-      option.disabled = true;
-    }
+    option.disabled = quantity === 0;
 
     productSelectBox.appendChild(option);
   });

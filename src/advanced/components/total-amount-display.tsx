8b215b27c6fdbc1dee03d7b2645e69260640654d
Doh Kim <ehgud456456@naver.com>
2025-04-17 11:25:04 +0900

    feat: 총액 디스플레이 레이아웃

--- src/advanced/components/total-amount-display.tsx
+++ src/advanced/components/total-amount-display.tsx
@@ -1,3 +1,25 @@
 export const TotalAmountDisplay = () => {
-  return <div id="cart-total" className="text-lg font-bold my-4"></div>;
+  return (
+    <div id="cart-total" className="text-lg font-bold my-4">
+      <TotalAmount />
+      <Discounts />
+      <Points />
+    </div>
+  );
+};
+
+export const TotalAmount = () => {
+  return `총액: ${0}원`;
+};
+
+export const Discounts = () => {
+  return <span className="text-red-500 ml-2">( % 할인 적용)</span>;
+};
+
+export const Points = () => {
+  return (
+    <span id="loyalty-points" className="text-blue-500 ml-2">
+      (포인트: )
+    </span>
+  );
 };

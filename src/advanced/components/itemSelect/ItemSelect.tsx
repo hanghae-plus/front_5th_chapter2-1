e28b9fada01f26accff58efe78a4142f359df7ad
minhee <jmh3699@naver.com>
2025-04-17 17:23:25 +0900

    feat: 상품 선택 컴포넌트에 change 핸들러 추가, 선택 옵션에 속성 추가 (dataset)

--- src/advanced/components/itemSelect/ItemSelect.tsx
+++ src/advanced/components/itemSelect/ItemSelect.tsx
@@ -1,15 +1,23 @@
-import { ItemListType } from 'src/advanced/types/ItemType.js';
+import { ChangeEvent } from 'react';
+import { useItem } from 'src/advanced/context/ItemContext.js';
 import ItemSelectOption from './ItemSelectOption.js';
-import { initialItemList } from '../../data/initialItemList.js';
 
 export default function ItemSelect() {
-  const itemList: ItemListType = initialItemList;
+  const {
+    state: { itemList },
+    setLastSelectedItem,
+  } = useItem();
+  const handleSelect = (e: ChangeEvent<HTMLSelectElement>) => {
+    const itemId = e.target.value;
+    setLastSelectedItem(itemId);
+  };
 
   return (
     <select
       name="상품 선택"
       id="product-select"
       className="border rounded p-2 mr-2"
+      onChange={handleSelect}
     >
       {itemList?.map((item) => <ItemSelectOption key={item.id} item={item} />)}
     </select>
--- src/advanced/components/itemSelect/ItemSelectOption.tsx
+++ src/advanced/components/itemSelect/ItemSelectOption.tsx
@@ -6,7 +6,12 @@ interface Props {
 
 export default function ItemSelectOption({ item }: Props) {
   return (
-    <option value={item.id} disabled={item.quantity === 0}>
+    <option
+      value={item.id}
+      disabled={item.quantity === 0}
+      title={item.name}
+      dataset-id={item.id}
+    >
       {item.name} - {item.price.toFixed(0)}원
     </option>
   );

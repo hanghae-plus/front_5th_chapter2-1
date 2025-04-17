111290db9ee879c4f7c2beaef5891ba407036d59
chloekim <chloe@perfitt.io>
2025-04-17 17:07:09 +0900

    feat:app.tsx, main.tsx추가

---
+++ src/advanced/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">🛒 장바구니 앱</h1>
+      {/* 여기에 CartPage 또는 메인 컴포넌트 넣기 */}
+    </div>
+  );
+};
+
+export default App;

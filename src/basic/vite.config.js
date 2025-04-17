8d9a5179884159d14e621ea9e312ca2648d782d6
JiYu <eunjun1018@gmail.com>
2025-04-17 12:55:29 +0900

    update

--- src/basic/vite.config.js
+++ src/basic/vite.config.js
@@ -2,10 +2,9 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
   base: '/hanghae99-chap4/basic/',
+  root: './src/basic',
   build: {
-    rollupOptions: {
-      input: 'src/basic/index.html', // index.html은 src/basic 안에 있어야 함
-    },
-    outDir: 'basic/dist', // 👈 루트 기준으로 dist 위치 조정
-  }
+    outDir: '../../../docs/basic', // 👈 완전히 정확한 위치
+    emptyOutDir: true,
+  },
 });

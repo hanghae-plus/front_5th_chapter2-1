2bae28118827c7a5dc8841c9168c7f3a7d7c1463
devchaeyoung <chaemaa00@gmail.com>
2025-04-18 04:44:24 +0900

    🚀chore: 배포 설정

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,34 @@
-import { defineConfig } from "vitest/config";
+import { defineConfig as defineTestConfig, mergeConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-export default defineConfig({
-  test: {
-    globals: true,
-    environment: "jsdom",
-    setupFiles: "src/setupTests.js",
-  },
-});
+export default ({ mode }) => {
+  const base = mode === 'production' ? '/front_5th_chapter1-3/' : '/';
+
+  return mergeConfig(
+    defineConfig({
+      plugins: [react()],
+      base,
+      build: {
+        rollupOptions: {
+          input: {
+            main: './index.html',
+            notFound: './404.html',
+          },
+        },
+      },
+    }),
+
+    defineTestConfig({
+      test: {
+        globals: true,
+        environment: 'jsdom',
+        setupFiles: './src/setupTests.ts',
+        coverage: {
+          reportsDirectory: './.coverage',
+          reporter: ['lcov', 'json', 'json-summary'],
+        },
+      },
+    }),
+  );
+};

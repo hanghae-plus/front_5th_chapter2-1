fa1d6915f67358bb779d7cad47075b36c9ac69f9
JiHwanLee <kdgosu53@gmail.com>
2025-04-18 00:24:30 +0900

    장바구니 계산 로직

--- src/advanced/components/TotalAmount.tsx
+++ src/advanced/components/TotalAmount.tsx
@@ -1,15 +1,35 @@
 import React from "react";
+import { cartService } from "../services/cartService";
+import { CartItem, Product } from "../types";
 
 interface TotalAmountProps {
-    children: number;
+    cartItems: CartItem[];
+    productList: Product[];
 }
 
-const TotalAmount = ({ children }: TotalAmountProps) => {
+const TotalAmount = ({ cartItems, productList }: TotalAmountProps) => {
+    // 여기서.. calcCart계산 결과를 넣어주여야 된다 .
+    const calculation = cartService.calculateCart(cartItems, productList);
+
+    // 얘는 일단 놔둬놓고..
+    // if (caculation.discountRate > 0) {
+    //     const discountRateSpan = document.createElement("span");
+    //     discountRateSpan.className = "text-green-500 ml-2";
+    //     discountRateSpan.textContent =
+    //         "(" + (caculation.discountRate * 100).toFixed(1) + "% 할인 적용)";
+    //     totalAmountEl.appendChild(discountRateSpan);
+    // }
+
     return (
         <div id="cart-total" className="text-xl font-bold my-4">
-            총액: {Math.round(children)}원
+            총액: {Math.round(calculation.totalAmount)}원
+            {calculation.hasDiscount && (
+                <span className="text-green-500 ml-2">
+                    ({calculation.discountPercentage.toFixed(1)}% 할인 적용)
+                </span>
+            )}
             <span id="loyalty-points" className="text-blue-500 ml-2">
-                (포인트: 0)
+                (포인트: {calculation.points})
             </span>
         </div>
     );

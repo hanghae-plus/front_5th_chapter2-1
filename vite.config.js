e64258af831eba705498a089a35b929dfc08c720
alchogh <alchogh01@gmail.com>
2025-04-18 01:54:13 +0900

    add : 배포 구현 test

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,21 @@
-import { defineConfig } from 'vitest/config';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+import { defineConfig } from 'vite';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
+  base: 'front_5th_chapter2-1',
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'basic/index.html'),
+      },
+    },
+    outDir: 'dist', // 기본값이지만 명시적으로 써도 좋아
+    emptyOutDir: true,
+  },
   test: {
     globals: true,
     environment: 'jsdom',

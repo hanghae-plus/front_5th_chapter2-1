5e8eb749982002dd9e26f693637c70e461aa566d
JiYu <eunjun1018@gmail.com>
2025-04-17 13:55:28 +0900

    deploy basic

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
-import { defineConfig } from 'vitest/config';
-import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
-  root: 'src/basic', // Vite의 루트 디렉토리를 src/basic으로 설정
-  base: '/hanghae99-chap4/basic/', // GitHub Pages에서의 접근 경로
+  base: '/hanghae99-chap4/basic/', // GitHub Pages에서의 배포 경로
   build: {
-    outDir: '../../../docs/basic', // 루트 기준으로 docs/basic에 빌드 결과 생성
-    emptyOutDir: true,
-    rollupOptions: {
-      input: resolve(__dirname, 'src/basic/index.html'), // 진입점 명시
-    },
+    outDir: 'docs/basic',  // 빌드된 파일들이 docs/basic 폴더에 저장되게끔
+    emptyOutDir: true,     // 빌드 전에 기존 파일들 삭제
   },
 });

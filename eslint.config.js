5f7fec22ded9d1350d7fd955a2416a510e973f0f
KumJungMin <rmawjdals@naver.com>
2025-04-17 18:33:06 +0900

    chore: eslint의 ts, react 설정 수정하기

--- eslint.config.js
+++ eslint.config.js
@@ -5,19 +5,35 @@ import tseslint from 'typescript-eslint';
 import prettierPlugin from 'eslint-plugin-prettier';
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  {
+    ignores: ['dist', 'node_modules'],
+  },
   {
     files: ['**/*.{ts,tsx}'],
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ['./tsconfig.json'],
+        ecmaFeatures: {
+          jsx: true,
+          tsx: true,
+        },
+      },
     },
     plugins: {
+      '@typescript-eslint': tseslint.plugin,
       'react-hooks': reactHooks,
       'react-refresh': reactRefresh,
       prettier: prettierPlugin,
     },
     rules: {
+      ...tseslint.configs.recommendedTypeChecked[1].rules,
       ...reactHooks.configs.recommended.rules,
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
       ...prettierPlugin.configs.recommended.rules,

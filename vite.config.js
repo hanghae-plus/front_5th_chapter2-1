e0cecf2c49269242e2364ad1ae68a724f4091b2a
JiYu <eunjun1018@gmail.com>
2025-04-21 12:11:03 +0900

    vite.config.js 설명 주석처리

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,10 @@ import { defineConfig } from 'vite';
 import { resolve } from 'path';
 
 export default defineConfig({
-  base: '/hanghae99-chap4/', // GitHub Pages에서의 배포 경로
+  base: '/hanghae99-chap4/', // 깃헙배포경로
   build: {
     rollupOptions: {
-      input: resolve(__dirname, 'basic/index.html'),  // index.html 경로 명시
+      input: resolve(__dirname, 'basic/index.html'),  // index.html 경로 알려주기
     },
     outDir: 'docs',  // 빌드된 파일들이 docs/basic 폴더에 저장되게끔
     emptyOutDir: true,     // 빌드 전에 기존 파일들 삭제
